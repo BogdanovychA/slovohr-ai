8e66bd9654884b8a3a7392e684b3aae1da220c8f
Andrii Bogdanovych <a@bogdanovych.org>
2026-05-18 11:52:02 +0300

    Зробив роботу асинхронною

--- src/ai/lapathoniia.py
+++ src/ai/lapathoniia.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from openai import OpenAI
+from openai import AsyncOpenAI
 
 from config import lapathoniia as l9a_config
 
@@ -10,29 +10,47 @@ logger = logging.getLogger(__name__)
 
 
 class Lapathoniia:
+
+    MAX_TOKENS = 1000
+    TEMPERATURE = 0.7
+
     def __init__(self, model: str):
 
-        self.client = OpenAI(
+        self.client = AsyncOpenAI(
             api_key=l9a_config.settings.key, base_url=l9a_config.settings.base_url
         )
 
         self.model = model
 
-    def query(self, system_prompt: str, user_prompt: str) -> str:
+    async def query(self, system_prompt: str, user_prompt: str) -> str:
 
-        response = self.client.chat.completions.create(
+        messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ]
+
+        response = await self.client.chat.completions.create(
             model=self.model,
-            messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": user_prompt},
-            ],
-            temperature=0.7,
-            max_tokens=1000,
+            messages=messages,
+            max_tokens=self.MAX_TOKENS,
+            temperature=self.TEMPERATURE,
+            n=1,
         )
 
-        return response.choices[0].message.content
+        message = response.choices[0].message
+        text = message.content.strip() if message.content else ""
+
+        return text
 
 
 if __name__ == "__main__":
-    l9a = Lapathoniia(l9a_config.settings.models["mamay"])
-    print(l9a.query("Використовуй Markdown-розмітку", "Розкажи про Україну"))
+    import asyncio
+
+    async def main():
+
+        l9a = Lapathoniia(l9a_config.settings.models["mamay"])
+        print(
+            await l9a.query("Використовуй закарпатський суржик", "Розкажи про Україну")
+        )
+
+    asyncio.run(main())
